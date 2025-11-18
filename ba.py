45235d754ecd5aee84ada3558e99fa128766a011
xuancm67khmt-afk <xuan.cm.67khmt@ntu.edu.vn>
2025-11-18 17:43:03 +0700

    bai_tap_tap1

---
+++ ba.py
@@ -0,0 +1,6 @@
+# Nhập hai số nguyên
+so1 = int(input("Nhập số nguyên thứ nhất: "))
+so2 = int(input("Nhập số nguyên thứ hai: "))
+
+# Xuất tổng và tích trong 1 dòng
+print(f"Tổng của {so1} và {so2} là {so1 + so2}, tích của chúng là {so1 * so2}")
