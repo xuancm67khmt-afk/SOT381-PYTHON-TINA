49e17423d3d8ef42fc449ae5acfbcf5cc4cff83b
xuancm67khmt-afk <xuan.cm.67khmt@ntu.edu.vn>
2025-12-15 16:39:41 +0700

    Square

---
+++ Square.py
@@ -0,0 +1,8 @@
+#Find the perimeter and area of a rectangle
+l = int(input(' Enter the length: '))
+w = int(input(' Enter the width: '))
+sum = l + w
+perimeter = sum * 2
+area = l * w
+print("Perimeter of the rectangle:", perimeter)
+print("Area of the rectangle:", area)
