99788cb0a27aae433cd05495bd685ac2b82ddb5a
Mỳ Xuân <xuan.cm.67khmt@ntu.edu.vn>
2025-12-22 14:47:21 +0700

    add TH2212_BAI2.py

---
+++ TH2212_BAI2.py
@@ -0,0 +1,17 @@
+a = int(input('Enter a number: '))
+b = int(input('Enter a number: '))
+c = int(input('Enter a number: '))
+max = a
+min = 0
+#Check the maximum value
+if b > max:
+    max = b
+if c > max:
+    max = c
+#Check the minimum value
+if b < min:
+    min = b
+if c < min:
+    min = c
+print('Maximum value: ', max)
+print('Minimum value: ', min)
