e4f7f1751ce855a5f1eb5fca7acab431028c68ce
Mỳ Xuân <xuan.cm.67khmt@ntu.edu.vn>
2025-12-22 14:21:40 +0700

    add TH15 Bai 3 W2

---
+++ TH2212_Bai11.py
@@ -0,0 +1,8 @@
+w = int(input('Enter a number: '))
+h = int(input('Enter a number: '))
+sum = w = h
+permiter = sum * 2
+area = w * h
+print('The perimeter of a rectangle: ', permiter)
+print('The area of a rectangle: ', area)
+
